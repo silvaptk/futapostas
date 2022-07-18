5d1744ece6d0f5fbe4f24afd63f71ee9d9242472
danlawand <daniel.lawand@usp.br>
2022-07-17 22:33:53 -0300

    createBet done

--- controllers/bets.js
+++ controllers/bets.js
@@ -2,7 +2,31 @@ exports.getBets = (req, res, next) => {}
 
 exports.getBet = (req, res, next) => {}
 
-exports.createBet = (req, res, next) => {}
+exports.createBet = async (req, res) => {
+    const { email, tipoAposta, valor, jogo_id } = req.body 
+    const newBet = new Bet(
+        email, 
+        tipoAposta,
+        valor,
+        resultado,
+        jogo_id
+    )
+
+    const result = await newBet.save()
+
+    if (result) {
+        return res.status(200).json({
+        message: "Aposta criada com sucesso",
+        payload: newBet
+        })
+    }
+
+    return res.status(400).json({
+        error: {
+        message: "Verifique os dados inseridos e tente novamente"
+        }
+    })
+}
 
 exports.updateBet = (req, res, next) => {}
 
--- models/bet.js
+++ models/bet.js
@@ -1,13 +1,46 @@
+const { query: postgresQuery } = require("../databases/postgres")
+const { run: runOnNeo4j } = require("../databases/neo4j")
+
 module.exports = class Bet {
-  constructor(id, email, nome, cpf, privacidadeDoPerfil) {
-    this.id = id;
+  constructor(email, tipoAposta, valor, jogo_id) {
     this.email = email;
-    this.nome = nome;
-    this.cpf = cpf;
-    this.privacidadeDoPerfil = privacidadeDoPerfil;
+    this.tipoAposta = tipoAposta;
+    this.valor = valor;
+    this.resultado = resultado;
+    this.jogo_id = jogo_id;
   }
 
-  save() {
-    
+  async save() {
+    try {
+      await runOnNeo4j(
+        `
+        MATCH 
+          (a:Usuário),
+          (b:Jogo)
+        WHERE a.email = ${this.email} AND
+              b.id = ${this.jogo_id}
+        MERGE (a)-[:${this.tipoAposta} ${this.resultado} ${this.valor}]->(b)
+        `
+      )
+    } catch (error) {
+      console.log(error);
+      return false
+    }
+
+    try {
+      await postgresQuery(
+        `
+          INSERT INTO 
+            Apostas (tipoAposta, valor, lucroOuPerda, montanteUsuario)
+          VALUES
+            (\'${this.tipoAposta}\', \'${this.valor}\', NULL, NULL) 
+        `
+      )
+    } catch (error) {
+      console.log(error);
+      return false
+    }
+
+    return true   
   }
 }
